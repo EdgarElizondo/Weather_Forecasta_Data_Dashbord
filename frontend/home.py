d14a39b899e924164aee12fc30f0ce9463a75743
EdgarElizondo <edgar_7_23@hotmail.com>
2024-08-07 12:40:08 -0600

    Add images to sky feature. Add dates in graph

--- frontend/home.py
+++ frontend/home.py
@@ -2,6 +2,10 @@ import streamlit as st
 import plotly.express as px
 from backend.src.dummy_data import get_data
 
+# CONSTANTS
+IMAGE_PATH = "frontend/src/images/"
+
+
 def home_page():
     st.title("Weather Forecast for the Next Days")
     place = st.text_input("Place: ")
@@ -12,10 +16,20 @@ def home_page():
     st.subheader(f"{option} for the next {days} days in {place}")
 
     if place:
-        dates, weather = get_data(place, days, option)
-        # Plot Graph
-        figure = px.line(x=dates, y=weather, labels={
-            "X":"Dates",
-            "Y":"Temperatures (C)"
-        })
-        st.plotly_chart(figure)
+        try:
+            weather_data = get_data(place, days)
+            # Plot Graph
+            if option == "Temperature":
+                temperature = [temp["main"]["temp"]/10 for temp in weather_data]
+                dates = [date["dt_txt"] for date in weather_data]
+                figure = px.line(x=dates, y=temperature, labels={
+                    "X":"Dates",
+                    "Y":"Temperatures (C)"
+                })
+                st.plotly_chart(figure)
+            elif option == "Sky":
+                weather = [sky["weather"][0]["main"] for sky in weather_data]
+                images_paths = [f"{IMAGE_PATH}{sky.lower()}.png" for sky in weather]
+                st.image(images_paths,width=100)
+        except KeyError:
+            st.text("Not Existing Place")
