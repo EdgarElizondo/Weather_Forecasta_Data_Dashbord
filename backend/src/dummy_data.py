39f8d1c297896998951f9c60d0ba46493898e50f
EdgarElizondo <edgar_7_23@hotmail.com>
2024-08-06 22:06:38 -0600

    Get temperature and sky data from API

--- backend/src/dummy_data.py
+++ backend/src/dummy_data.py
@@ -1,20 +1,29 @@
 import os
 import requests
 
-# API KEY
-#API_KEY = os.getenv("f008cfcf53ef01d493df6bc5ac3d76c3")
-API_KEY = "f008cfcf53ef01d493df6bc5ac3d76c3"
+# CONSTANTS
+API_KEY = os.getenv("API_WeatherMap")
+MEASURE_TIME_INTERVAL = 3
+CONVERSION_DAY_PARAMETER = int(24 / MEASURE_TIME_INTERVAL)
+
 
 def get_data(place:str, days:int, kind:str):
+    # Get API Data    
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     req = requests.get(url)
 
     dates = ["2022-25-10","2022-26-10","2022-27-10"]
-    temperatures = [10, 11, 15]
+    weather = [10, 11, 15]
 
-    print(req.json())
+    data = []
+    json_data = req.json()
+    if kind == "Temperature":
+        data = [json_data["list"][day]["main"]["temp"]/10 for day in range(days * CONVERSION_DAY_PARAMETER)]
+    elif kind == "Sky":
+        data = [json_data["list"][day]["weather"][0]["main"] for day in range(days * CONVERSION_DAY_PARAMETER)]
 
-    return dates, temperatures
+    print(data)
+    return dates, weather
 
 if __name__ == "__main__":
     get_data("Tokio", 1, "Sky")
--- frontend/home.py
+++ frontend/home.py
@@ -5,16 +5,17 @@ from backend.src.dummy_data import get_data
 def home_page():
     st.title("Weather Forecast for the Next Days")
     place = st.text_input("Place: ")
-    days = st.slider("Forecast Days", min_value=1, max_value=5,
-                    help="Select the number of forecasted days")
+    days = int(st.slider("Forecast Days", min_value=1, max_value=5,
+                    help="Select the number of forecasted days"))
     option = st.selectbox("Select data to view", 
                         ("Temperature", "Sky"))
     st.subheader(f"{option} for the next {days} days in {place}")
 
-    dates, temperatures = get_data(place, days, option)
-    # Plot Graph
-    figure = px.line(x=dates, y=temperatures, labels={
-        "X":"Dates",
-        "Y":"Temperatures (C)"
-    })
-    st.plotly_chart(figure)
+    if place:
+        dates, weather = get_data(place, days, option)
+        # Plot Graph
+        figure = px.line(x=dates, y=weather, labels={
+            "X":"Dates",
+            "Y":"Temperatures (C)"
+        })
+        st.plotly_chart(figure)
